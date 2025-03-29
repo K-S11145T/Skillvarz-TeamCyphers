542e0ad3a5ac0356ecb527b4da67f3d3cc13c604
K-S11145T <tanushsingh843@gmail.com>
2025-03-29 14:03:55 +0530

    Landingpage

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LandingPage from './components/LandingPage'
+
+const App = () => {
+  return (
+    <div className='w-full h-screen bg-red-950'>
+      <LandingPage />
+    
+    </div>
+  )
+}
+
+export default App
