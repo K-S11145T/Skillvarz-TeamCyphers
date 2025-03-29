19e9c0c35a70b50c3b7402fa21fafb1396536a30
K-S11145T <tanushsingh843@gmail.com>
2025-03-29 15:06:34 +0530

    Page2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import LandingPage from './components/LandingPage'
+import Page2 from './components/Page2'
 
 const App = () => {
   return (
     <div className='w-full h-screen bg-red-950'>
       <LandingPage />
+      <Page2 />
     
     </div>
   )
