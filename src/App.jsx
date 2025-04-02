bdf73380267284a909278e9cab99a7e2e7065b10
itsvikash93 <vikashkumar827070@gmail.com>
2025-04-02 18:07:07 +0530

    landing page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import LandingPage from './components/LandingPage'
-import Page2 from './components/Page2'
-import Page3 from './components/Page3'
-import Page4 from './components/Page4'
+import React from "react";
+import LandingPage from "./components/LandingPage";
+import Page2 from "./components/Page2";
+import Page3 from "./components/Page3";
+import Page4 from "./components/Page4";
+import ReactLenis, { useLenis } from "lenis/react";
 
 const App = () => {
+  const lenis = useLenis();
   return (
-    <div className='w-full h-screen bg-red-950'>
-      <LandingPage />
-      <Page2 />
-      <Page3 />
-      <Page4 />
-    
-    </div>
-  )
-}
+    <ReactLenis root>
+      <div className="w-full h-screen bg-red-950 select-none font-[orbitron]">
+        <LandingPage />
+        <Page2 />
+        <Page3 />
+        <Page4 />
+      </div>
+    </ReactLenis>
+  );
+};
 
-export default App
+export default App;
