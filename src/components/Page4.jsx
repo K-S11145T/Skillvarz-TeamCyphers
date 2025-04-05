85f236e822d2914acda1384466e0b6acc3761745
ᴠɪᴠᴇᴋ <vivekfr28@gmail.com>
2025-04-05 13:37:14 +0530

    color and dimensions fix

--- src/components/Page4.jsx
+++ src/components/Page4.jsx
@@ -11,7 +11,7 @@ const Page5 = () => {
           className="w-fit h-fit object-contain"
         />
 
-        <h1 className="text-[#C65244] text-5xl"><DecryptedText
+        <h1 className="text-[#E35E4E] text-5xl"><DecryptedText
           text="Behind the scenes"
           speed={50}
           revealDirection='center'
@@ -30,8 +30,8 @@ const Page5 = () => {
           alt=""
         />
 
-        <div className="w-full absolute text-white top-0 left-0  flex h-full border-2 border-[#C65244]">
-          <div className="w-[25%] flex items-end p-5 border-x-[1px] border-[#C65244]  h-full ">
+        <div className="w-full absolute text-white top-0 left-0  flex h-full border-2 border-[#E35E4E]">
+          <div className="w-[25%] flex items-end p-5 border-x-[1px] border-[#E35E4E]  h-full ">
             <div className=" w-full">
               <h1 className=" ml-2 text-2xl font-bold">Soundtracks</h1>
               <p className="text-base ml-2 mt-2  ">
@@ -48,34 +48,34 @@ const Page5 = () => {
                 />
               </div>
               <div className="flex text-black items-center justify-evenly">
-                <button className="bg-red-500 w-fit [clip-path:polygon(0%_0%,95%_0%,100%_20%,100%_100%,5%_100%,0%_80%)] font-orbitron font-bold px-3 py-2">
+                <button className="bg-[#E35E4E] w-fit [clip-path:polygon(0%_0%,95%_0%,100%_20%,100%_100%,5%_100%,0%_80%)] font-orbitron font-bold px-3 py-2">
                   EXPLORE
                 </button>
-                <button className="bg-red-500 w-fit [clip-path:polygon(0%_0%,95%_0%,100%_20%,100%_100%,5%_100%,0%_80%)] font-orbitron font-bold px-3 py-2">
+                <button className="bg-[#E35E4E] w-fit [clip-path:polygon(0%_0%,95%_0%,100%_20%,100%_100%,5%_100%,0%_80%)] font-orbitron font-bold px-3 py-2">
                   STEAM
                 </button>
               </div>
             </div>
           </div>
 
-          <div className="w-[25%] border-x-[1px] overflow-hidden group relative border-[#C65244] flex flex-col items-center justify-center h-full ">
-            <div className="absolute w-full h-full left-0 top-[100%] group-hover:top-0 duration-300 bg-[#C65244]">
+          <div className="w-[25%] border-x-[1px] overflow-hidden group relative border-[#E35E4E] flex flex-col items-center justify-center h-full ">
+            <div className="absolute w-full h-full left-0 top-[100%] group-hover:top-0 duration-300 bg-[#E35E4E]">
               {" "}
             </div>
             <h1 className="text-xl absolute">Weapon Design</h1>
             <h1 className="text-2xl absolute right-5 bottom-3 font-bold">01</h1>
           </div>
 
-          <div className="w-[25%] relative overflow-hidden group border-x-[1px] flex flex-col items-center justify-center  border-[#C65244]  h-full ">
-            <div className="absolute w-full h-full left-0 top-[100%] group-hover:top-0 duration-300 bg-[#C65244]">
+          <div className="w-[25%] relative overflow-hidden group border-x-[1px] flex flex-col items-center justify-center  border-[#E35E4E]  h-full ">
+            <div className="absolute w-full h-full left-0 top-[100%] group-hover:top-0 duration-300 bg-[#E35E4E]">
               {" "}
             </div>
             <h1 className="text-xl absolute">Developer Interviews</h1>
             <h1 className="text-2xl absolute right-5 bottom-3 font-bold">02</h1>
           </div>
 
-          <div className="w-[25%] relative border-x-[1px] group overflow-hidden flex flex-col items-center justify-center border-[#C65244]  h-full ">
-            <div className="absolute w-full h-full left-0 top-[100%] group-hover:top-0 duration-300 bg-[#C65244]">
+          <div className="w-[25%] relative border-x-[1px] group overflow-hidden flex flex-col items-center justify-center border-[#E35E4E]  h-full ">
+            <div className="absolute w-full h-full left-0 top-[100%] group-hover:top-0 duration-300 bg-[#E35E4E]">
               {" "}
             </div>
             <h1 className="text-xl absolute">Soundtracks</h1>
